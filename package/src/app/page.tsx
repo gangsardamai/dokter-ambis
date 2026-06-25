42ea7792a883101bf79161802bfcde12766d5028
gangsardamai <gangsardamai17@gmail.com>
2026-06-25 12:55:20 +0700

    test homepage

--- package/src/app/page.tsx
+++ package/src/app/page.tsx
@@ -1,26 +1,7 @@
-import React from "react";
-import Hero from "@/app/components/Home/Hero";
-import Courses from "@/app/components/Home/Courses";
-import Universities from "@/app/components/Home/Universities";
-import Mentor from "@/app/components/Home/Mentor";
-import Testimonial from "@/app/components/Home/Testimonials";
-import ContactForm from "@/app/components/ContactForm";
-
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Dokter Ambis",
-};
-
 export default function Home() {
   return (
-    <main>
-      <Hero />
-      <Universities />
-      <Courses />
-      <Mentor />
-      <Testimonial />
-      <ContactForm />
-          </main>
+    <main className="container py-20">
+      <h1>Dokter Ambis Berhasil Deploy</h1>
+    </main>
   );
 }
